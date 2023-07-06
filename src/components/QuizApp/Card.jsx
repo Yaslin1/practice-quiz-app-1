3e70eb1e9b30446fe6c5b8638a74b4e59f59d246
Yaslin Carmona <yaslincarmona@Yaslins-MacBook-Air.local>
2023-07-06 10:31:16 -0400

    practice-quiz-app-1

---
+++ src/components/QuizApp/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card () {
+    return (
+        <div className="card">
+        <p>1/3</p>
+        <h2>Card</h2>
+        <p>options</p>
+        </div>
+    )
+}
---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,12 @@
+import AppLayout from "../layout/AppLayout"
+import QuizApp from "../components/QuizApp/QuizApp"
+
+export default function () {
+
+    return(
+        <AppLayout>
+        <h2>Quiz Page</h2>
+        <QuizApp />
+        </AppLayout>
+    )
+}
